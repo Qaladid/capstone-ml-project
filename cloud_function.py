fb22dac3d883487c2978bf5301488fe8f469a130
Qaladid <abdiaziz11410@gmail.com>
2025-01-14 03:10:03 +0300

    added the model files and the cloud_function scripts

---
+++ cloud_function.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np
+from PIL import Image
+import requests
+from io import BytesIO
+import tflite_runtime.interpreter as tflite
+
+
+# Function to download the image from the URL
+def download_image_from_url(url):
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+    img = img.convert('RGB')  # Convert to RGB to ensure 3 channels
+    img = img.resize((224, 224), Image.NEAREST)  # Resize the image
+    return img
+
+
+# Preprocess the image as expected by the model
+def preprocess_input(x):
+    x /= 127.5
+    x -= 1.
+    return x
+
+
+# Load and process the image
+def predict_image_from_url(url, model_path='DensNet_v5_06_0.960.keras.tflite'):
+    # Download the image from URL
+    img = download_image_from_url(url)
+    
+    # Convert the image to numpy array
+    x = np.array(img, dtype='float32')
+    X = np.array([x])
+    
+    # Preprocess the image
+    X = preprocess_input(X)
+    
+    # Load the TensorFlow Lite model
+    interpreter = tflite.Interpreter(model_path=model_path)
+    interpreter.allocate_tensors()
+    
+    # Get input and output tensor details
+    input_index = interpreter.get_input_details()[0]['index']
+    output_index = interpreter.get_output_details()[0]['index']
+    
+    # Run inference
+    interpreter.set_tensor(input_index, X)
+    interpreter.invoke()
+    preds = interpreter.get_tensor(output_index)
+    
+    # Extract the probability
+    probability = preds[0][0]  # Assuming binary classification
+    
+    # Decode the probability to a binary label
+    binary_label = int(probability > 0.5)
+    
+    # Map binary labels to class names
+    class_labels = {0: 'NORMAL', 1: 'PNEUMONIA'}
+    predicted_class = class_labels[binary_label]
+    
+    # Return the result
+    return predicted_class, probability
+
+
+# Example usage:
+url = 'https://w-abdit-x-ray-classification-p-162f9eae96324f11ae1d7edcea2f9a90.community.saturnenterprise.io/view/examples/examples/tensorflow/person100_bacteria_475.jpeg'
+
+predicted_class, probability = predict_image_from_url(url)
+print(f"Predicted class: {predicted_class}, Probability: {probability}")
+
+
+
+
+
+
